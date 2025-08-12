b9e1989b98ab77ee72c2e5538d174dcc628e9556
mhdiamiri <officialmehdiamiri@gmail.com>
2025-08-12 19:10:38 +0330

    first project created

---
+++ HelloOSGi/src/helloosgi/Activator.java
@@ -0,0 +1,24 @@
+package helloosgi;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+		System.out.println("Hello OSGi");
+	}
+
+	public void stop(BundleContext bundleContext) throws Exception {
+		Activator.context = null;
+		System.out.println("Goodbye OSGi");
+	}
+
+}
